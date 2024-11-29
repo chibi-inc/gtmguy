643123775f0f495252995129855e353e72aa412f
RiyaMathew-11 <riyapmathew2000@gmail.com>
2024-11-29 14:50:54 +0530

    chore: add `clarity` tracker script

--- pages/app.vue
+++ pages/app.vue
@@ -226,6 +226,17 @@ useHead({
   ],
   title: 'GTMGuy Dashboard',
   description: 'Create and manage your go-to-market strategies with GTMGuy.',
+
+  script: [
+  {
+      type: 'text/javascript',
+      children: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "p56ykifasc");`
+    }
+  ]
 })
 
 // Add user state
